4c7500c39e28399e6cadd7981a64a361ef07ccac
00UnNamed00 <38175238+00UnNamed00@users.noreply.github.com>
2021-01-12 10:18:18 +0700

    fix

--- Project2/profile.php
+++ Project2/profile.php
@@ -20,7 +20,7 @@ $result2 = mysqli_fetch_assoc($result);
 </head>
 
 <body>
-    <img class="logo" src="logo.svg" alt="" style="margin-top:-5px">
+    <img class="logo" src="logo.svg" alt="" style="margin-top:-50px">
     <div class="animation-area">
         <ul class="box-area">
             <li></li>
@@ -34,20 +34,11 @@ $result2 = mysqli_fetch_assoc($result);
     <div class="mainprofil">
         <p class="sign" align="center">My Profile</p>
         <?php echo '<img src="'.$_SESSION["user_image"].'" class="img-responsive img-circle img-thumbnail profil" />';?>
-        <?php if(isset($_SESSION['gagal_login'])) {
-			echo "<p class='error' align='center'>".$_SESSION['gagal_login']."</p>";
-		} ?>
         <form class="form3" action="" method="POST">
             <div class="tulisan"> <label>Username</label></div>
-            <input class="un" type="text" name="username" align="center" placeholder="Username" value="<?php echo $result2['username']; ?>">
+            <input class="un" type="text" name="username" align="center" placeholder="Username" value="<?php echo $result2['username']; ?>" disabled>
             <div class="tulisan"> <label>Email</label></div>
-            <input class="un" type="text" name="email" align="center" placeholder="Email" value="<?php echo $result2['email']; ?>">
-            <div class="tulisan"> <label>Password</label></div>
-            <input class="pass" type="password" name="password1" align="center" placeholder="Password" id="password" value="<?php echo $result2['password']; ?>">
-            <label class="show"> Show Password
-			<input type="checkbox" name="checked" id="checkbox">
-				<span class="checkmark"></span>
-			</label>
+            <input class="un" type="text" name="email" align="center" placeholder="Email" value="<?php echo $result2['email']; ?>" disabled>
             <button class="submit" name="submit" type="submit" align="center">CONFIRM</button>
             <center>
                 <p><a href="index.php"><ins>Back</ins></a></p>
